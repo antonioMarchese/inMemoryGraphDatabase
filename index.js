c69e276b81b17375ca4178ab98a857ac68fde50b
Antonio Marchese <01645194400@estudante.ufjf.br>
2023-10-13 12:24:23 -0300

    adding pipetypes manipulations

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ Dagoba.error = function (error_message) {
   return false;
 };
 
+Dagoba.simpleTraversal = function (dir) {
+  var find_method = dir == "out" ? "findOutEdges" : "findInEdges";
+  var edge_list = dir == "out" ? "_in" : "_out";
+
+  return function (graph, args, gremlin, state) {
+    if (!gremlin && (!state.edges || !state.edges.length)) return "pull"; // query initialization
+
+    if (!state.edges || !state.edges.length) {
+      state.gremlin = gremlin;
+      state.edges = graph[find_method](gremlin.vertex).filter(
+        Dagoba.filterEdges(args[0])
+      );
+    }
+
+    if (!state.edges.length) return "pull";
+
+    var vertex = state.edges.pop()[edge_list];
+    return Dagoba.gotoVertex(state.gremlin, vertex);
+  };
+};
+
 Dagoba.G = {}; // Prototype
 
 Dagoba.Q = {}; // Query Prototype
+
+Dagoba.Pipetypes = {};
 
 Dagoba.graph = function (Vertices, Edges) {
   // Factory
@@ -25,7 +48,7 @@ Dagoba.graph = function (Vertices, Edges) {
   return graph;
 };
 
-Dagoba.query = function (grap) {
+Dagoba.query = function (graph) {
   // Factory
   var query = Object.create(Dagoba.Q);
 
@@ -37,8 +60,19 @@ Dagoba.query = function (grap) {
   return query;
 };
 
-// Graph manipulation
+Dagoba.addPipetype("vertex", function (graph, args, gremlin, state) {
+  if (!state.vertices) state.vertices = graph.findVertices(args); // state initialization
 
+  if (!state.vertices.length) return "done"; // all done
+
+  var vertex = state.vertices.pop();
+  return Dagoba.makeGremlin(vertex, gremlin.state);
+});
+
+Dagoba.addPipetype("out", Dagoba.simpleTraversal("out"));
+Dagoba.addPipetype("in", Dagoba.simpleTraversal("in"));
+
+// Graph manipulation
 Dagoba.G.addVertices = function (vertices) {
   vertices.forEach(this.addVertex.bind(this));
 };
@@ -75,11 +109,37 @@ Dagoba.G.addEdge = function (edge) {
   this.edges.push(edge);
 };
 
-// Query manipulation
+Dagoba.G.v = function () {
+  var query = Dagoba.query(this);
+  query.add("vertex", [].slice.call(arguments));
+  return query;
+};
 
+// Query manipulation
 Dagoba.Q.add = function (pipetype, args) {
   // add a new step to the query
   var step = [pipetype, args];
   this.program.push(step);
   return this;
+};
+
+// Pipetype manipulation
+Dagoba.addPipetype = function (name, fun) {
+  Dagoba.Pipetypes[name] = fun;
+  Dagoba.Q[name] = function () {
+    return this.add(name, [].slice.apply(arguments));
+  };
+};
+
+Dagoba.fauxPipetype = function (_, _, maybe_gremlin) {
+  // Pass the result upstream or send a pull downstream
+  return maybe_gremlin || "pull";
+};
+
+Dagoba.getPipetype = function (name) {
+  var pipetype = Dagoba.Pipetypes[name];
+
+  if (!pipetype) Dagoba.error("Unrecognized pipetype: " + name);
+
+  return pipetype || Dagoba.fauxPipetype;
 };
